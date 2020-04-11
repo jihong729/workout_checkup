855319ebc13656ec2ab9bf7e6bff2e81319a153b
Jihong MIn <sammin@Samui-MacBookPro.local>
2020-04-11 22:23:28 +0900

    Errors in widgets/alerts.dart file. Experiencing trouble refactoring this Alert

--- lib/widgets/alerts.dart
+++ lib/widgets/alerts.dart
@@ -1,1 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
+class Alerts extends StatefulWidget {
+  @override
+  _AlertsState createState() => _AlertsState();
+}
+
+class _AlertsState extends State<Alerts> {
+  @override
+  Widget build(BuildContext context) {
+    Alert(
+        style: kAlertStyle,
+        context: context,
+        title: "Awesome!",
+        desc: "You finished the Goal for $workoutName.",
+        image: Image.asset('images/complete.png'),
+        buttons: [
+          DialogButton(
+            child: Text(
+              'COOL',
+              style: kAlertButtonTextStyle,
+            ),
+            color: Colors.grey.shade700,
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ]).show();
+  }
+}
